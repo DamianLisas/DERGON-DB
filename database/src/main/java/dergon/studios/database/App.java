c6a184d4055983ab51d07a63faad02d634e5efca
Damian Lisas <damian.lisas@gmail.com>
2018-11-20 23:12:21 -0300

    Commit del proyecto base

---
+++ database/src/main/java/dergon/studios/database/App.java
@@ -0,0 +1,13 @@
+package dergon.studios.database;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
